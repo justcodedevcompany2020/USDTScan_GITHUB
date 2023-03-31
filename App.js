90d75e9f5121f6df678db57e1110856b45e35c8e
musho777 <129147695+musho777@users.noreply.github.com>
2023-03-31 14:36:59 +0400

    header ios & android

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import 'react-native-gesture-handler';
 import Navigation from './src/navigation';
 import {Provider} from 'react-redux';
 import {store} from './src/store/configStore';
+import { Platform, StatusBar } from 'react-native';
 
 export default function App() {
   return (
     <Provider store={store}>
+      <StatusBar  translucent = {Platform.OS ==='ios'}  />
       <Navigation />
     </Provider>
   );
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,9 +5,12 @@ import {
   SafeAreaView,
   StyleSheet,
   TouchableOpacity,
+  View,
 } from 'react-native';
 import {Svgs} from '../Svg/svg';
 import img from '../images/logo.png';
+
+
 
 const windowHeight = Dimensions.get('window').height;
 
@@ -18,11 +21,18 @@ export const Header = ({onPress}) => {
       style={[
         stayles.header,
         {backgroundColor: '#e3eee9', height:Platform.OS ==='ios'? windowHeight /8.5:windowHeight /11},
-      ]}>
-      <TouchableOpacity onPress={onPress} style={Platform.OS ==='ios'? stayles.icon:stayles.androidicone}>
-        <Svgs />
-      </TouchableOpacity>
-      <Image style={stayles.logo} source={img} />
+      ]}
+      forceInset={{
+        bottom: 'always'
+      }}
+      >
+        <View style={Platform.OS ==='ios'? stayles.icon:stayles.androidicone}>
+          <TouchableOpacity   onPress={onPress} > 
+              <Svgs />
+          </TouchableOpacity>
+      </View>
+
+        <Image style={stayles.logo} source={img} />
     </SafeAreaView>
   );
 };
@@ -35,19 +45,22 @@ const stayles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
+    position:'relative',
   },
   icon: {
     position: 'absolute',
-    left: 20,
-    bottom: -windowHeight / 70,
-    width: 50,
-    height: 50,
+    left:20,
+    justifyContent:'center',
+    height:'100%',
+    bottom:0,
+  },
+  IconView:{
+    flex:1,    
   },
   androidicone:{
     position: 'absolute',
     left: 20,
-    width: 50,
-    height: 50,
+    height:'100%',
     justifyContent:'center',
   }
 });
