daa40b084c15e418d0964fd3e312cb3606d52370
musho777 <129147695+musho777@users.noreply.github.com>
2023-04-26 15:14:10 +0400

    webView

--- src/Pages/profili.js
+++ src/Pages/profili.js
@@ -1,24 +1,48 @@
 import { useSelector } from "react-redux"
 import { WebView } from 'react-native-webview';
 import { deleteToken } from "../store/action/action";
+import { useRef } from "react";
 
 export const Profile = ({navigation}) =>{
     const {auth} = useSelector(st=>st)
-    let webviewRef = null
-    const sendMessage = () =>{
-        const message = 'Hello from react native'
-        webviewRef.postMessage(message)
+    let webviewRef = useRef(null)    
+ 
+    const config = {
+      headers:{APP_STATUS:'from app'}
     }
+
+    const postData = 'param1=value1&param2=value2';
+
+  const postUrl = 'https://example.com';
+
+  const handleButtonClick = () => {
+        `$.ajax({
+        type: "POST",
+        url: "https://usdtscan.com",
+        data:" app_access_val=121",
+        success: function(ex) {
+          alert(ex)
+        }})`
+  };
+
+
     return <WebView
-    reg = {(ref) =>{webviewRef = ref}}
+    ref = {webviewRef}
     useWebKit={true}
+    // onLoad={() => sendPostRequest()}
+    // injectJavaScript  = {handleButtonClick}
     source={{ uri:`https://usdtscan.com/?api_access_key=${auth.token}`}}
+    injectedJavaScript={ `$.ajax({
+      type: "POST",
+      url: "https://usdtscan.com",
+      data:" app_access_val=121",
+      success: function(ex) {
+        alert(ex)
+      }})`}
     onNavigationStateChange={(webViewState)=>{
-        console.log(webViewState.url)
         let logour_url = "https://usdtscan.com/start_app?out"
         if(webViewState.url.search(logour_url) !== -1 )
         {   
-            console.log('aaaaaaaaaaaaaaaaaa')
             dispatch(deleteToken())
             navigation.navigate('Home')
         }
